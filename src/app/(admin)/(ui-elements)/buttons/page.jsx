a6f827d091b218e546e42c1c16ed9c3ae41185f7
BasiratHamayoon <basirathumayun@gmail.com>
2026-09-09 10:19:01 +0500

    remove the unwanted imports

--- src/app/(admin)/(ui-elements)/buttons/page.jsx
+++ src/app/(admin)/(ui-elements)/buttons/page.jsx
@@ -2,7 +2,6 @@ import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import Button from "@/components/ui/button/Button";
 import { BoxIcon } from "@/icons";
-import { Metadata } from "next";
 import React from "react";
 
 export const metadata = {
